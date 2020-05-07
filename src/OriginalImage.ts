9c54a137415553f2131fb69c07eaffee18cbf9a1
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-05-07 19:11:10 +0200

    Commented : OriginalImage.ts :book:

--- src/OriginalImage.ts
+++ src/OriginalImage.ts
@@ -1,15 +1,33 @@
 /**
+ * OriginalImage class
  * 
+ * @author Lucas Fridez <lucas.fridez@he-arc.ch>
  */
 
-import { Canvas } from "./AbstractChart";
+// Imports
+import { Canvas } from "./Canvas";
 
+/**
+ * OriginalImage class to draw a source image in a canvas
+ */
 export class OriginalImage extends Canvas {
 
+    /**
+     * Instanciate a new OriginalImage
+     * @param idCanvasChart id canvas
+     * 
+     * @author Lucas Fridez <lucas.fridez@he-arc.ch>
+     */
     public constructor(idCanvasChart: string) {
         super(idCanvasChart);
     }
 
+    /**
+     * Draw an image in a canvas
+     * @param buffer buffer image source
+     * 
+     * @author Lucas Fridez <lucas.fridez@he-arc.ch>
+     */
     public drawImage = async (buffer: string | ArrayBuffer) => {
 
         return new Promise((resolve, reject) => {
