85ba881b25d4c65ffd44c7b7bfa302c6decd168d
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-06-18 16:08:23 +0200

    Updated : GrayscaledImage.ts and comments

--- src/GrayscaleImage.ts
+++ src/GrayscaleImage.ts
@@ -41,18 +41,14 @@ export class GrayscaleImage extends Canvas {
 
         var arraylength = canvasImage.width * canvasImage.height * 4;
 
-        //Common formula for converting to grayscale.
-        //gray = 0.3*R + 0.59*G + 0.11*B
-
-        // Create density array
-        var density: Array<number> = new Array<number>();
-        for (let i = 0; i < 256; i++) {
-            density[i] = 0;
-        }
+        // Create density array filled with 0
+        var density: Array<number> = new Array<number>().fill(0);
 
         // Fill density array
         for (var i = arraylength - 1; i > 0; i -= 4) {
-            var gray = 0.3 * data[i - 3] + 0.59 * data[i - 2] + 0.11 * data[i - 1];
+            //Common formula for converting to grayscale.
+            //gray = 0.3*R + 0.59*G + 0.11*B
+            let gray: number = 0.3 * data[i - 3] + 0.59 * data[i - 2] + 0.11 * data[i - 1];
 
             density[gray.toFixed(0)] = density[gray.toFixed(0)] + 1;
 
@@ -64,7 +60,7 @@ export class GrayscaleImage extends Canvas {
         //Display the output image
         this.context.canvas.width = canvasImage.width;
         this.context.canvas.height = canvasImage.height;
-        this.context.putImageData(imageData, 0, 0);        
+        this.context.putImageData(imageData, 0, 0);
 
         return density;
     }
